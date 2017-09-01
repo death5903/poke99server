3fca3bd5b3757ec35c799b77bdc2f09b55910546
Ted <condingmaster@gmail.com>
2017-09-01 10:23:41 +0800

    444

--- src/poke99server/gameobj/GameMessage.java
+++ src/poke99server/gameobj/GameMessage.java
@@ -2,17 +2,10 @@ package poke99server.gameobj;
 
 public class GameMessage {
 
-<<<<<<< HEAD
 	private String action = "n";
 	private String data = "n";
 	private String data2 = "n";
-
-=======
-	private String action = "none";
-	private String data = "none";
-	private String data2 = "none";
 	
->>>>>>> a78e5ee7b7b91bbb6389b61745ce9ffeb9f7d1a7
 	public String getAction() {
 		return action;
 	}
@@ -28,7 +21,6 @@ public class GameMessage {
 	public void setData(String data) {
 		this.data = data;
 	}
-<<<<<<< HEAD
 
 	public String getData2() {
 		return data2;
@@ -37,15 +29,5 @@ public class GameMessage {
 	public void setData2(String data2) {
 		this.data2 = data2;
 	}
-
-=======
-	public String getData2() {
-		return data2;
-	}
-	public void setData2(String data2) {
-		this.data2 = data2;
-	}
->>>>>>> a78e5ee7b7b91bbb6389b61745ce9ffeb9f7d1a7
-	
 
 }
--- src/poke99server/gameobj/MessageEncoder.java
+++ src/poke99server/gameobj/MessageEncoder.java
@@ -22,20 +22,10 @@ public class MessageEncoder implements Encoder.Text<GameMessage> {
 
 	@Override
 	public String encode(GameMessage message) throws EncodeException {
-<<<<<<< HEAD
-
+		
 		JsonObject jObj = Json.createObjectBuilder().add("action", message.getAction()).add("data", message.getData())
 				.add("data2", message.getData2()).build();
 
-=======
-		
-		JsonObject jObj = Json.createObjectBuilder()
-				.add("action", message.getAction())
-				.add("data", message.getData())
-				.add("data2", message.getData2())
-				.build();
-		
->>>>>>> a78e5ee7b7b91bbb6389b61745ce9ffeb9f7d1a7
 		return jObj.toString();
 	}
 
